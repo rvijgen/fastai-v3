aae3c59312c600e6a92703ce6a67c41c92a7735d
Richard Vijgen <mail@richardvijgen.nl>
2020-07-01 13:59:11 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,20 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/n00kcvkfqqfm4uh/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['BERL',
+  'BERX',
+  'BLOM',
+  'BOON',
+  'BOSC',
+  'BRIN',
+  'CNAI',
+  'CUBA',
+  'CUCO',
+  'CUYZ',
+  'DUDO']
 path = Path(__file__).parent
 
 app = Starlette()
